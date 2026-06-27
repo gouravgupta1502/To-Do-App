93f3890027fec86659910a44da8b563e35f0275b
Gourav <gouravgupta1502@gmail.com>
2026-06-27 11:20:28 +0530

    Create basic GUI without interactions, Day 16

--- functions.py
+++ functions.py
@@ -10,3 +10,6 @@ def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
 
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
